39217f3d899cb5064f66bad06638b0860ad28b51
Sam Raab <raabsm@gmail.com>
2019-08-26 11:03:48 -0400

    added ReflectionTest which prints contents of each field in a class

--- src/sam/ExampleClass.java
+++ src/sam/ExampleClass.java
@@ -1,6 +1,17 @@
 package sam;
-import java.io.Serializable;
 
-public class ExampleClass implements Serializable {
+public class ExampleClass {
 
+	private int i;
+	private String s;
+	private int k = 5;
+	public ExampleClass(){
+		i = 7;
+		s = "hello";
+	}
+	
+	public int geti(){
+		return i;
+	}
+	
 }
---
+++ src/sam/ReflectionTest.java
@@ -0,0 +1,39 @@
+package sam;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
+public class ReflectionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer e = new Integer(5);
+		Class c = e.getClass();
+		Field[] fields = c.getDeclaredFields();
+		for(int i =0; i< fields.length; i++){
+			Object[] fieldValue = new Object[1];
+			fields[i].setAccessible(true);
+			try {
+					fieldValue[0] = fields[i].get(e);
+				} catch (IllegalArgumentException e1) {
+					// TODO Auto-generated catch block
+					fieldValue[0] = 0;
+					e1.printStackTrace();
+				} catch (IllegalAccessException e1) {
+					// TODO Auto-generated catch block
+					fieldValue[0] = 0;
+					e1.printStackTrace();
+				}
+				System.out.println(fields[i].toString() + ", value: " + Arrays.deepToString(fieldValue));
+		}
+		Field i;
+//		try {
+//			i = c.getDeclaredField("i");
+//			i.setAccessible(true);
+//			i.set(e, 5543);
+//		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e2) {
+//			// TODO Auto-generated catch block
+//			e2.printStackTrace();
+//		}
+	}
+
+}
